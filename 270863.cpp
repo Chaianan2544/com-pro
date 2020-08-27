0aaf93994674a5581d95e1615a9e2c18185ab5c0
Chaianan2544 <69625661+Chaianan2544@users.noreply.github.com>
2020-08-27 14:18:09 +0700

    27-08-63

---
+++ 270863.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<time.h>
+using namespace std;
+int main(void)
+{
+	srand(time(NULL));
+	int rn =rand()%10;
+	int mn;
+	int n=0;
+	do
+	{
+		n=n+1;
+		cout<<"Enter your number "<<n<<" : ";
+		cin>>mn;
+		if(mn==rn)
+		{
+			cout<<"random number is "<<rn<<endl;
+			cout<<"Yep same"<<endl;
+		}
+		else if(mn>rn)
+		{
+			cout<<"wrong!!\nyour number is too high"<<endl;
+		}
+		else
+			cout<<"wrong!!\nyour number is too low"<<endl;
+	}
+	while(mn!=rn);
+	cout<<"Exit program"<<endl;
+
+
+
+
+
+
+
+
+
+	/*======================================
+	srand(time(NULL));
+	int RanNum = 5+(rand()%6);
+	cout<<RanNum<<endl;
+	int MyNum;
+	cout<<"Enter your number : ";
+	cin>>MyNum;
+	if(MyNum>RanNum)
+	{
+		cout<<"MyNum > RanNum"<<endl;
+	}
+	else if(MyNum<RanNum)
+	{
+		cout<<"MyNum < RanNum"<<endl;
+	}
+	else
+	{
+		cout<<"MyNum = RanNum"<<endl;
+	}
+	========================================*/
+	return(0);
+}
