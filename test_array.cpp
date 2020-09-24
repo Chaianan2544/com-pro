170847712f43a7ee720297d9653e2e938563fe26
Chaianan2544 <69625661+Chaianan2544@users.noreply.github.com>
2020-09-24 16:00:59 +0700

    Add files via upload

---
+++ test_array.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	float numray[10]={20,10,32,35,84,75,66,97,68,99}; 
+	//=====================
+	float max=0;
+	for(int i=0;i<10;i++)
+	{
+		if(max<numray[i])
+		{
+			max=numray[i];
+		}
+	}
+	cout<<"Max in array : "<<max<<endl;
+	//=====================
+	float min=numray[0];
+	for(int i=0;i<10;i++)
+	{
+		if(min>numray[i])
+		{
+			min=numray[i];
+		}
+	}
+	cout<<"Min in array : "<<min<<endl;
+	//=====================
+
+	float total=0;
+	for(int i=0;i<10;i++)
+	{
+		total+=numray[i];
+	}
+	cout<<"Average in array : "<<total/10<<endl;
+
+	//======================
+	cout<<"Show data in array index 0 => 9"<<endl;
+	for(int i=0;i<10;i++)
+	{
+		cout<<"index["<<i<<"] = "<<numray[i]<<endl;
+	}
+	cout<<"=================================="<<endl;
+	cout<<"Show data in array index 9 => 0"<<endl;
+	for(int i=9;i>=0;i--)
+	{
+		cout<<"index["<<i<<"] = "<<numray[i]<<endl;
+	}
+	return(0);
+}
